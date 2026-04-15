462d9e679957c52b4d20a322341e0c216ef90c2b
achraf boumessouer <achraf.boumessouer@ardeotis.fr>
2026-04-15 14:15:17 +0200

    init: initial spring boot project setup

---
+++ src/test/java/com/example/ardeotis_platform/ArdeotisPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ardeotis_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdeotisPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
