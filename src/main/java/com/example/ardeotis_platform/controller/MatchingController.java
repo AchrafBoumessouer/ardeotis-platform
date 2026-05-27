bc7139af3952eb556dc3df8f7602403706506b83
achraf boumessouer <achraf.boumessouer@ardeotis.fr>
2026-05-27 16:51:39 +0200

    feat: add matching score algorithm

---
+++ src/main/java/com/example/ardeotis_platform/controller/MatchingController.java
@@ -0,0 +1,25 @@
+package com.example.ardeotis_platform.controller;
+
+import com.example.ardeotis_platform.dto.response.MatchingResultDto;
+import com.example.ardeotis_platform.service.MatchingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/matching")
+@RequiredArgsConstructor
+public class MatchingController {
+
+    private final MatchingService matchingService;
+
+    @GetMapping("/missions/{missionId}/consultants")
+    public List<MatchingResultDto> matchConsultantsToMission(@PathVariable UUID missionId){
+        return matchingService.matchConsultantsToMission(missionId);
+    }
+}
--- src/main/java/com/example/ardeotis_platform/service/MatchingService.java
+++ src/main/java/com/example/ardeotis_platform/service/MatchingService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -28,6 +29,23 @@ public class MatchingService {
     }
 
     private MatchingResultDto calculateMatch(Mission mission, Consultant cons) {
+        int score = 0;
+        if(cons.getStatus() == ConsultantStatus.AVAILABLE){
+            score += 20;
+        }
+        if(cons.getStatus() == ConsultantStatus.AVAILABLE){
+            score += 20;
+        }
+        score += calculateSkilsScore(mission.getSkills(),cons.getSkills());
         return null;
+    }
+
+    private int calculateSkilsScore(Set<String> missionSkills, Set<String> consultantSkills){
+        if(missionSkills == null || missionSkills.isEmpty() || consultantSkills == null || consultantSkills.isEmpty()){
+            return 0;
+        }
+
+        long matchedskill = missionSkills.stream().filter(ms -> consultantSkills.stream().anyMatch((cs -> cs.equalsIgnoreCase(ms)))).count();
+        return (int) ((matchedskill * 50)/missionSkills.size());
     }
 }
